52710f0ace28fe6111eaa256d4a21b07bb890194
officexchangesg <officexchangesg@gmail.com>
2022-11-29 12:03:44 +0800

    Delete dj_knowledge directory

--- dj_knowledge/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class DjKnowledgeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    verbose_name = 'Knowledge'
-    name = 'dj_knowledge'
